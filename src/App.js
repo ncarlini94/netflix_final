b6e48a5e73fb9521729032980c5b080356affd50
ncarlini94 <ncarlini94@gmail.com>
2023-06-10 00:13:01 -0300

    Reparacion del App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 import { HomePage,
         SingInPage,
         SingUpPage,
