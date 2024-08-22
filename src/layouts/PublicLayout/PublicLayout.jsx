82e793477a1a24e90ecb24b26448560be447893c
Nico <ncarlini94@gmail.com>
2024-08-22 16:20:57 -0300

    Update background logo welcome

--- src/layouts/PublicLayout/PublicLayout.jsx
+++ src/layouts/PublicLayout/PublicLayout.jsx
@@ -35,7 +35,7 @@ const PublicLayout = () => {
         style={{
           width:'100%',
           backgroundImage: path !== '/signup' ?
-          `url(https://assets.nflxext.com/ffe/siteui/vlv3/5eab1b22-c5ea-48b0-8ef4-862b3fa6df2c/c9fed70b-9392-4823-ac65-cc55c369f6dc/AR-en-20230724-popsignuptwoweeks-perspective_alpha_website_small.jpg)`
+          `url(https://assets.nflxext.com/ffe/siteui/vlv3/36a4db5b-dec2-458a-a1c0-662fa60e7473/4ce87c6a-9742-41a0-84db-b2610f4d448d/AR-es-20240820-TRIFECTA-perspective_WEB_c2dc0d09-eb32-42ca-9711-1c4e51e89d3d_medium.jpg)`
           : 'none',
           boxShadow: path !== '/signup' ? 'inset 0 0 0 2000px rgba(0,0,0,0.56)': 'none'
         }}>
