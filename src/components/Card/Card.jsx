c83839b42ecc1d7df41ea89ad1e0f6a03ac74fa6
Nico <ncarlini94@gmail.com>
2025-08-14 22:29:22 -0300

    Update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Card.module.css";
 import apiBuilder from "../../hooks/getApi";
 import { Link } from "react-router-dom";
--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router";
 import apiBuilder from "../../hooks/getApi";
 import ErrorVideo from '../../assets/imagen/Error.jpg'
 import styles from './VideoCard.module.css'
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player'
 
 
 const VideoCard = ({ language}) => {
